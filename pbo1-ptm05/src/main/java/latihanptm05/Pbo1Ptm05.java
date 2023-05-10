e5207cc812d015d4ff304376f45e36aa286d65c4
Kissaputera <mkissaputera@gmail.com>
2023-05-10 15:48:33 +0800

    Create Pbo1Ptm05.java

---
+++ pbo1-ptm05/src/main/java/latihanptm05/Pbo1Ptm05.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package latihanptm05;
+
+/**
+ *
+ * @author ACER
+ */
+public class Pbo1Ptm05 {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
